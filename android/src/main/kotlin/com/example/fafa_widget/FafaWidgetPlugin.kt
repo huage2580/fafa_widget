5c4bd7679bbf1a6eb14bc0e7bcccd21e9898cfa2
hua <2838666797@qq.com>
2022-08-02 11:31:29 +0800

    尝试miui状态栏

--- android/src/main/kotlin/com/example/fafa_widget/FafaWidgetPlugin.kt
+++ android/src/main/kotlin/com/example/fafa_widget/FafaWidgetPlugin.kt
@@ -1,48 +1,93 @@
 package com.example.fafa_widget
 
+import android.app.Activity
 import android.content.Context
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+
 /** FafaWidgetPlugin */
-class FafaWidgetPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private var context:Context? = null
+class FafaWidgetPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private var context: Context? = null
+    private var activity: Activity? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "fafa_widget")
-    channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.applicationContext
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    when (call.method) {
-        "getPlatformVersion" -> {
-          result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        }
-        "update" -> {
-          if (context!=null){
-            FaFaWidgetProvider.updateWidget(context!!)
-          }
-          result.success("done")
-        }
-        else -> {
-          result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "fafa_widget")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getPlatformVersion" -> {
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
+            "update" -> {
+                if (context != null) {
+                    FaFaWidgetProvider.updateWidget(context!!)
+                }
+                result.success("done")
+            }
+            "statusBarIconLight" -> {
+                light()
+                result.success("")
+            }
+            "statusBarIconDark"->{
+                dark()
+                result.success("")
+            }
+            else -> {
+                result.notImplemented()
+            }
         }
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-    context = null
-  }
+    private fun dark(){
+        val window = activity?.window ?: return
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun light(){
+        val window = activity?.window ?: return
+        val flag: Int = window.decorView
+            .systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        window.decorView.systemUiVisibility = flag
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        context = null
+    }
+
+    override fun onAttachedToActivity(p0: ActivityPluginBinding) {
+        activity = p0.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReattachedToActivityForConfigChanges(p0: ActivityPluginBinding) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
 }
