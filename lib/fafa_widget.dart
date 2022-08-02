c089877ebeb69e33fd09f88b55766f1cad016daf
hua <2838666797@qq.com>
2022-08-02 11:56:26 +0800

    Revert "状态栏？"
    This reverts commit 06ab8b214bfda5d4aba73d51045cfad90e26da4b.

--- lib/fafa_widget.dart
+++ lib/fafa_widget.dart
@@ -15,14 +15,4 @@ class FafaWidget {
     final String? version = await _channel.invokeMethod('update');
     return version;
   }
-
-  static Future<String?> light() async{
-    final String? version = await _channel.invokeMethod('statusBarIconLight');
-    return version;
-  }
-
-  static Future<String?> dark() async{
-    final String? version = await _channel.invokeMethod('statusBarIconDark');
-    return version;
-  }
 }
